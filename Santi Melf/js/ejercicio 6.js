0846aab5ee4343c211f6ad33bda823d2384030e4
melfi-santi <stormtroopersan07@gmail.com>
2023-06-05 16:31:23 -0300

    hoa

--- Santi Melf/js/ejercicio 6.js
+++ Santi Melf/js/ejercicio 6.js
@@ -1,9 +1,11 @@
+function ejercicio6(){
 var par_o_impar = numero
     if (numero % 2 == 0){
-        return "El número es par.";
+        console.log ("El número es par.");
    } else{
-        return "El número es impar."
+        console.log ("El número es impar.");
    }
 numero = int(input("Ingrese un número entero: "));
 resultado = par_o_impar(numero);
 print(resultado);
+}
