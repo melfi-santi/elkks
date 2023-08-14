3e3b0e5a701614de24f91e67d811368cf50eedbe
melfi-santi <stormtroopersan07@gmail.com>
2023-08-14 17:15:59 -0300

    1

--- Santi Melf/php/form-modificacion.php
+++ Santi Melf/php/form-modificacion.php
@@ -7,7 +7,7 @@ include 'menu.php';
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Formulario de Modificacion</title>
-	<link rel="stylesheet" href="css/formAltas.css">
+	<link rel="stylesheet" href="../css/formAltas.css">
 </head>
 <body>
 
