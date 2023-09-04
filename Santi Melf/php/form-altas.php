5f2ccb5c85eb14f5d6fe21115423e1ecb2b173d1
dominatris <stormtroopersan07@gmail.com>
2023-09-04 17:36:16 -0300

    1

--- Santi Melf/php/form-altas.php
+++ Santi Melf/php/form-altas.php
@@ -33,7 +33,7 @@ include 'menu.php';
 		<div class="grupoInputs">
 		<label for="foto">Archivo</label>
 		<input type="file" name="foto"><br>
-		<img src="">
+		<img src="../"></img>
 	    </div>
 
 		<div class="btn-submit">
